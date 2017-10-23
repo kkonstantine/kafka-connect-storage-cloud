31ebe29fd103b131483eee5648adb498d33d1dbb
Brandt Newton <brandt.newton@coxautoinc.com>
2017-10-23 13:41:30 -0400

    fixed style

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/avro/AvroDataConfig.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/avro/AvroDataConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.confluent.connect.s3.format.avro;
 
 import io.confluent.connect.storage.common.ComposableConfig;
@@ -17,8 +33,9 @@ import static io.confluent.connect.avro.AvroDataConfig.SCHEMAS_CACHE_SIZE_DEFAUL
 import static io.confluent.connect.avro.AvroDataConfig.SCHEMAS_CACHE_SIZE_DOC;
 
 /**
- * Adapts {@link io.confluent.connect.avro.AvroDataConfig} which uses {@link io.confluent.common.config.AbstractConfig} which is
- * incompatible with {@link org.apache.kafka.common.config.AbstractConfig}.
+ * Adapts {@link io.confluent.connect.avro.AvroDataConfig} which uses {@link
+ * io.confluent.common.config.AbstractConfig} which is incompatible with {@link
+ * org.apache.kafka.common.config.AbstractConfig}.
  */
 public class AvroDataConfig extends AbstractConfig implements ComposableConfig {
 
